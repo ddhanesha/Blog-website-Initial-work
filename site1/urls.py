deef56cb2dd15d079785a2924351d94b1952da62
dip <dip.dhanesha@gmail.com>
2015-07-01 16:19:11 +0530

    3rd commit

--- site1/urls.py
+++ site1/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url,patterns
 
-from site1.views import post_list
+from site1.views import post_list,post_detail
 
 urlpatterns = patterns('',
         url(r'^post_list/', post_list),
+        url(r'post_detail/(?P<pk>[0-9]+)/',post_detail),
 )
