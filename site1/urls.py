bb6b20f07ae2249b4c61a615ac031a196464d2bd
dip <dip.dhanesha@gmail.com>
2015-07-01 18:03:43 +0530

    4th commit

--- site1/urls.py
+++ site1/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url,patterns
 
-from site1.views import post_list,post_detail
+from site1.views import post_list,post_detail,post_new,post_edit
 
 urlpatterns = patterns('',
         url(r'^post_list/', post_list),
-        url(r'post_detail/(?P<pk>[0-9]+)/',post_detail),
+        url(r'^post_detail/(?P<pk>[0-9]+)/',post_detail),
+        url(r'^post_new/',post_new),
+        url(r'^/(?P<pk>[0-9]+)/post_edit/', post_edit),
 )
--- site1/views.py
+++ site1/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404, redirect
 from django.utils import timezone
 from site1.models import Post
+from site1.forms import PostForm
 
 
 def post_list(request):
@@ -9,7 +10,23 @@ def post_list(request):
 
 def post_detail(request,pk):
     Post.objects.get(pk=pk)
-    post= get_object_or_404(Post, pk=pk)
-    return render(request,'post_list.html', {'post':post})
 
+    post= get_object_or_404(Post, pk=pk)
+    return render(request,'post_detail.html', {'post':post})
+
+def post_new(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('site1.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    form = PostForm()
+    print form
+    return render(request, 'post_new.html', {'form':form})
 # Create your views here.
